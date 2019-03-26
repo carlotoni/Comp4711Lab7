4457753baa6a7e271dde4496df14254d60c263f3
tonihe <tonihe7@gmail.com>
2019-03-26 11:36:50 -0700

    initial client setup

--- app/Controllers/Client.php
+++ app/Controllers/Client.php
@@ -2,8 +2,26 @@
 namespace App\Controllers;
 use CodeIgniter\Controller;
 use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\HTTP\CURLRequest;
+
+use App\Models\WorkModel;
+
+$client = new CURLRequest(
+    new \Config\App(),
+    new \CodeIgniter\HTTP\URI(),
+    new \CodeIgniter\HTTP\Response(new \Config\App()),
+    $options
+);
+
+$response = $client->request('GET', 'localhost:8080', [
+    'user_input' => ['name', 'quantity', 'price']
+]);
+
+$this->input->get(array('name', 'quantity', 'price'));
 
 class Client extends Controller
 {
     use ResponseTrait;
+
+    
 }
